0e278013ea344cfc0ea77316c79dd1d2529e1fea
zerl <starss@outlook.com>
2019-10-04 09:46:35 +0800

    Concurrency with shared Variables

---
+++ gopl/ch9/memo/memo5/memo_test.go
@@ -0,0 +1,25 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package memo_test
+
+import (
+	"./memo"
+	"testing"
+
+	"../memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Concurrent(t, m)
+}
